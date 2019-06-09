b6e667e9258d85f7f129b7f7772d17af458092e8
alisherKak <alisher.alish333@gmail.com>
2019-06-09 13:29:26 +0600

    Добавьте файлы проекта.

---
+++ HomeWork17_06_19/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork17_06_19
+{
+    public class BankAccount
+    {
+        public int _cash { get; set; } = 0;
+        private object lockObject = new object();
+
+        public void AddMoney()
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(5* new Random().Next(100));
+                _cash += 20;
+                Show();
+                Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine();
+            }
+        }
+
+        public void RemoveMoney()
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(5 * new Random().Next(100));
+                _cash -= 20;
+                Show();
+                Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine();
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(_cash);
+        }
+    }
+}
---
+++ HomeWork17_06_19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork17_06_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount();
+
+            BankAccoutCashChange(bankAccount);
+
+            Console.ReadLine();
+            (bankAccount as BankAccount).Show();
+            Console.ReadLine();
+        }
+
+        static void BankAccoutCashChange(BankAccount bankAccount)
+        {
+            var threads = new Thread[100];
+
+            for (int i = 0; i < 50; i++)
+            {
+                threads[i] = new Thread(bankAccount.AddMoney);
+            }
+
+            for (int i = 50; i < 100; i++)
+            {
+                threads[i] = new Thread(bankAccount.RemoveMoney);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+        }
+    }
+}
